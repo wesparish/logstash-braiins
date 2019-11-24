d109203db61d3962c18d2d4cd00802118ad0b377
Wes Parish <wes@elastiscale.net>
2019-11-24 14:06:33 +0000

    Updating to include new IPs

--- scripts/settings.py
+++ scripts/settings.py
@@ -2,7 +2,7 @@ import os
 
 EXTRA_VARIABLES = {
     'IP_PREFIX': os.getenv('IP_PREFIX', '10.2.2.'),
-    'LAST_OCTET_START': int(os.getenv('LAST_OCTET_START', 199)),
+    'LAST_OCTET_START': int(os.getenv('LAST_OCTET_START', 198)),
     'LAST_OCTET_END': int(os.getenv('LAST_OCTET_END', 229))+1,
     'ES_HOST': os.getenv('ES_HOST', 'http://10.2.2.231:9200')
 }
